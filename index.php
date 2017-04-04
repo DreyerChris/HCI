09dc2687bc2d4c64af44de735631fa1756e727a7
Chris Dreyer <christiaanldreyer@gmail.com>
2017-04-04 19:06:59 +0200

    Added Base Skeleton

--- index.php
+++ index.php
@@ -2,10 +2,41 @@
 <html>
     <head>
         <title>Welcome to NuMetro!</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="CSS/style.css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
-    <body>
-        <h1>Test</h1>
+    <body class="main">
+        <div id="navbar">
+            <div id="iconbar" class="col-xs-2"></div>
+            <div class="col-xs-10" id="navbartext">NuMetro</div>
+        </div>
+        
+        <!--Carousel--><div id="carouselmock">Carousel</div>
+        
+        <div id="loginbar" class="col-xs-12">
+            <div id="loginlink" class="col-xs-6">
+                <button class="loginlinkbtn">Log In</button>
+            </div>
+            <div id="signuplink" class="col-xs-6">
+                <button class="signuplinkbtn">Sign Up</button>
+            </div>
+        </div>
+        <div id="quicklinks">
+            <div class="row">
+                <div id="quicklink" class="col-xs-6">
+                    <button>Buy Tickets</button>
+                </div>
+                <div id="quicklink" class="col-xs-6">
+                    <button>Cinemas</button>
+                </div>
+            </div>
+            <div class="row">
+                <div id="quicklink" class="col-xs-12">
+                    <button>Movies</button>
+                </div>
+            </div>
+        </div>
+        
+        <!--Scrollable imagelist--><div id="imagescrolllistmock">Image Scroll List</div>
     </body>
 </html>
