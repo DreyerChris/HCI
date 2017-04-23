94172617803b95c055bf78ce322f2165935a3891
Chris <christiaanldreyer@gmail.com>
2017-04-23 17:00:54 +0200

    Added movie rating to movie page

--- moviepage.php
+++ moviepage.php
@@ -57,42 +57,49 @@ if ($conn->connect_error) {
                         echo '<div class="row"';
                             echo'<div class="col-xs-12" id="movieposter">';
 
-                                echo'<iframe width="100%" height="500" src="https://www.youtube.com/embed/h9y6oPka3us" frameborder="0" allowfullscreen></iframe>';
+                                echo'<iframe width="100%" height="400" src="https://www.youtube.com/embed/h9y6oPka3us" frameborder="0" allowfullscreen></iframe>';
                             echo'</div>';
                      echo'</div>';
 
                      echo'<div id="moviepageinfo" class="col-xs-12">';
                         echo'<div>';
-                             echo'<h3 id="moviepagetitle">'.$row["nameMovie"].'</h3><div id="agerestrictionmoviepage">PG V</div><div id="favouritecontainer"><span id="favouriteempty" class="glyphicon glyphicon-heart"></span></div>';
+                             echo'<h3 id="moviepagetitle">'.$row["nameMovie"].'</h3><div id="agerestrictionmoviepage">PG V</div><div id="genre">Animation</div><div id="favouritecontainer"><span id="favouriteempty" class="glyphicon glyphicon-heart"></span></div>';
                                 /*echo'<dl>';
                              echo '<dt>Director: '.$row["Director"].'</dt>';
                              echo '<dt>Music: '.$row["Music"].'</dt>';
                              echo '<dt>Running time: '.$row["runningTime"].'</dt>';
                                 echo'</dl>';*/
                         echo'</div>';
-                        echo '<div class="col-xs-6">';
-                            echo '<div class="row">';
-                                echo'<h3 class="first" id="movieinfoheadermoviepage">Director: <span id="movieinfocontentmoviepage">'.$row["Director"].'</span></h3>';
+                        echo'<div class="row">';
+                            echo '<div class="col-xs-6">';
+                                echo '<div class="row">';
+                                    echo'<h3 class="first" id="movieinfoheadermoviepage">Director: <span id="movieinfocontentmoviepage">'.$row["Director"].'</span></h3>';
+                                echo '</div>';
+                                echo '<div class="row">';
+                                    echo'<h3 id="movieinfoheadermoviepage">Music: <span id="movieinfocontentmoviepage">'.$row["Music"].'</span></h3>';
+                                echo '</div>';
+                                echo '<div class="row">';
+                                    echo'<h3 class="first" id="movieinfoheadermoviepage">Running time: <span id="movieinfocontentmoviepage">'.$row["runningTime"].'</span></h3>';
+                                echo '</div>';
+                                echo '<div class="row">';
+                                    echo'<h3 id="movieinfoheadermoviepage">Release date: <span id="movieinfocontentmoviepage">14 February</span></h3>';
+                                echo '</div>';
                             echo '</div>';
-                            echo '<div class="row">';
-                                echo'<h3 id="movieinfoheadermoviepage">Music: <span id="movieinfocontentmoviepage">'.$row["Music"].'</span></h3>';
-                            echo '</div>';
-                            echo '<div class="row">';
-                                echo'<h3 class="first" id="movieinfoheadermoviepage">Running time: <span id="movieinfocontentmoviepage">'.$row["runningTime"].'</span></h3>';
-                            echo '</div>';
-                            echo '<div class="row">';
-                                echo'<h3 id="movieinfoheadermoviepage">Release date: <span id="movieinfocontentmoviepage">14 February</span></h3>';
+                            echo '<div class="col-xs-6">';
+                                echo '<p id="moviedescription"><span id="blurb">Movie blurb.</span><br>' .$row["synopsis"]. '</p>';
                             echo '</div>';
                         echo '</div>';
-                        echo '<div class="col-xs-6">';
-                            echo '<p id="moviedescription"><span id="blurb">Movie blurb.</span><br>' .$row["synopsis"]. '</p>';
-                        echo '</div>';
+                        echo'<div id="movieratingrow" class="row">';
+                            echo'<div class="col-xs-2"><img height="70" width="120" src="Images/IMDB/IMDB_Logo.png"/></div>';
+                            echo'<div id="movieratingtext" class="col-xs-2">8/10</div>';
+                        echo'</div>';
+                        echo'<div class="row">';
+                            echo'<div id="ratingstars" class="col-xs-12"><span id="ratingstar" class="glyphicon glyphicon-star"><span id="ratingstar" class="glyphicon glyphicon-star"><span id="ratingstar" class="glyphicon glyphicon-star"><span id="ratingstar" class="glyphicon glyphicon-star"><span id="ratingstar" class="glyphicon glyphicon-star-empty"></span></div>';
+                        echo'</div>';
                      echo'</div>';
 
-                     echo'<div class="col-xs-12">';
-                        echo'<div class="white">';
-                            echo'<h2>Book tickets</h2>';
-                        echo'</div>';
+                     echo'<div id="moviedropdowns" class="col-xs-12">dropdowns';
+                        
                      echo'</div>';
                     }
                 }
